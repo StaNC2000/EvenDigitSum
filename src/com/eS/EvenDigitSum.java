3756e2b7bc02cf6a453592dbeadb3ca44b5b7691
es <StaNC2000>
2020-01-24 11:47:18 +0100

    Method getEvenDigitSum created using while loop, tested

--- src/com/eS/EvenDigitSum.java
+++ src/com/eS/EvenDigitSum.java
@@ -2,11 +2,26 @@ package com.eS;
 
 public class EvenDigitSum {
 
-    public static int getEvenDigitSum(int number){
-        return - 1;
-    }
+    public static int getEvenDigitSum(int number) {
+        int digit = number;
+        int sum = 0;
+        int reminder = 0;
 
+        if (number < 0) {
+            return -1;
+        }
+
+        while (digit > 0) {
+            reminder = digit % 10;
+            digit = digit / 10;
+            if (reminder % 2 == 0) {
+                sum = sum + reminder;
+            }
+        }
+        return sum;
+    }
     public static void main(String[] args) {
 	// write your code here
+        System.out.println(getEvenDigitSum(252));
     }
 }
