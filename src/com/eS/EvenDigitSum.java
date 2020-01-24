6d5247c41262f2c778d4f73509eb6132c66005bf
es <StaNC2000>
2020-01-24 10:36:25 +0100

    Initial method getEvenDigitSum

---
+++ src/com/eS/EvenDigitSum.java
@@ -0,0 +1,12 @@
+package com.eS;
+
+public class EvenDigitSum {
+
+    public static int getEvenDigitSum(int number){
+        return - 1;
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
